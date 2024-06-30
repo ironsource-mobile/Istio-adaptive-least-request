5800295550a9c009f8353dfecba62e21e4994d25
liorf <lior.franko@ironesrc.com>
2024-06-30 15:40:29 +0300

    Change adpative to adaptive

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -27,7 +27,7 @@ import (
 	"istio-adaptive-least-request/test/utils"
 )
 
-const namespace = "istio-adpative-least-request-system"
+const namespace = "istio-adaptive-least-request-system"
 
 var _ = Describe("controller", Ordered, func() {
 	BeforeAll(func() {
@@ -60,7 +60,7 @@ var _ = Describe("controller", Ordered, func() {
 			var err error
 
 			// projectimage stores the name of the image used in the example
-			var projectimage = "example.com/istio-adpative-least-request:v0.0.1"
+			var projectimage = "example.com/istio-adaptive-least-request:v0.0.1"
 
 			By("building the manager(Operator) image")
 			cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", projectimage))
